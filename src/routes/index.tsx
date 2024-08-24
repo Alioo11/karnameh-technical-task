f6985dc7f3e73f583ee09bc386dfbfc0533445a4
Alioo11 <110mors110@gmail.com>
2024-08-24 21:50:15 +0330

    add `use-query-param` router adapter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,9 @@ import { Suspense, lazy, type FC } from "react";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import Loader from "@components/Kit/Loader";
 import Layout from "@components/Layout";
-
+import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6";
 import { ROUTES } from "@constants/routes";
+import { QueryParamProvider } from "use-query-params";
 
 const _404 = lazy(() => import("@components/Pages/404"));
 const Dashboard = lazy(() => import("@components/Pages/Dashboard"));
@@ -12,13 +13,17 @@ const Router: FC = () => {
   return (
     <Suspense fallback={<Loader />}>
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path={ROUTES.ROOT} element={<Navigate to={ROUTES.DASHBOARD} replace />} />
-            <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-            <Route path="*" element={<_404 />} />
-          </Routes>
-        </Layout>
+        <QueryParamProvider
+          adapter={ReactRouter6Adapter}
+        >
+          <Layout>
+            <Routes>
+              <Route path={ROUTES.ROOT} element={<Navigate to={ROUTES.DASHBOARD} replace />} />
+              <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+              <Route path="*" element={<_404 />} />
+            </Routes>
+          </Layout>
+        </QueryParamProvider>
       </BrowserRouter>
     </Suspense>
   );
