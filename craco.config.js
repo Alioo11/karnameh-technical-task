148f19e79880e0f34a674c51a86d5c5019b206f6
Alioo11 <110mors110@gmail.com>
2024-08-24 21:41:14 +0330

    feat: add service ana hook paths to craco and tsconfig

--- craco.config.js
+++ craco.config.js
@@ -2,11 +2,12 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@hooks": path.resolve(__dirname, "src/hooks/"),
       "@helpers": path.resolve(__dirname, "src/helpers/"),
       "@_types": path.resolve(__dirname, "src/types/"),
       "@components": path.resolve(__dirname, "src/components/"),
       "@constants": path.resolve(__dirname, "src/constants/"),
+      "@services": path.resolve(__dirname, "src/services/"),
     },
   },
 };
