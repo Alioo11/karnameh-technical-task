444276548c68f505caf7ae8ebbc2bfbc9b023a11
Alioo11 <110mors110@gmail.com>
2024-08-24 21:46:32 +0330

    feat: update layout component(removed unused containers)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,15 @@
 import Container from "@mui/material/Container";
+import { Box } from "@mui/material";
+import { grey } from "@mui/material/colors";
 import type { HOCFunctionalComponent } from "@_types/components";
 
 const Layout: HOCFunctionalComponent = ({ children }): JSX.Element => {
   return (
-    <Container maxWidth="xl" sx={{ p: "1rem", pt: 0 }}>
-      {children}
-    </Container>
+    <Box p={0} m={0} bgcolor={grey["100"]} height="100vh">
+      <Container maxWidth="xl" sx={{ p: "1rem", bgcolor: "white" , height:"100%" }}>
+        {children}
+      </Container>
+    </Box>
   );
 };
 export default Layout;
