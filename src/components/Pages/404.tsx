5ae64b58f23b5414f090143a0421e5f0682f3fed
Alioo11 <110mors110@gmail.com>
2024-08-24 21:46:09 +0330

    feat: implement 404 page

--- src/components/Pages/404.tsx
+++ src/components/Pages/404.tsx
@@ -1,5 +1,14 @@
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
 const _404Page = () => {
-  return <div>404</div>
+  return (
+    <Stack justifyContent="center" alignContent="center" height="100%" width="100%">
+      <Typography textAlign="center" variant="h3">
+        404 Not Found.
+      </Typography>
+    </Stack>
+  );
 };
 
 export default _404Page;
